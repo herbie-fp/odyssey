8c051aa9d7c69860b01fadc7d1cdd34ef7b82779
Edward Misback <elmisback@gmail.com>
2022-08-12 19:52:01 -0700

    Add another exclamation point (update testing).

--- src/extension.ts
+++ src/extension.ts
@@ -25,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('interactive-herbie.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Herbie!!');
+		vscode.window.showInformationMessage('Hello World from Herbie!!!');
 	});
 
 	context.subscriptions.push(disposable);

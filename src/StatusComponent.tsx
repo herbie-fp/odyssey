358da9f55bd70e84674d4bc7bbfbfa10f63e79ab
Rich Chen <richchen2002@gmail.com>
2023-06-28 10:39:02 -0700

    Allow user to choose IP

--- src/StatusComponent.tsx
+++ src/StatusComponent.tsx
@@ -4,11 +4,12 @@ const timeBetweenChecks = 3000; // Time between checking for the status, in mill
 
 function ServerStatusComponent() {
     const [status, setStatus] = useState<number | null>(null);
+    const [inputIP, setInputIP] = useState<string>('127.0.0.1:8000');
 
     useEffect(() => {
         const fetchStatus = async () => {
             try {
-                const response = await fetch('http://127.0.0.1:8000/up');
+                const response = await fetch(`http://${inputIP}/up`);
                 setStatus(response.status);
             } catch (error) {
                 setStatus(null);
@@ -18,10 +19,22 @@ function ServerStatusComponent() {
         fetchStatus();
         const intervalId = setInterval(fetchStatus, timeBetweenChecks);
         return () => clearInterval(intervalId);
-    }, []);
+    }, [inputIP]);
+
+    const handleIPChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputIP(event.target.value);
+    };
 
     return (
         <div>
+            <label htmlFor="ipInput">Host (Herbie):</label>
+            <input
+                type="text"
+                id="ipInput"
+                value={inputIP}
+                onChange={handleIPChange}
+            />
+
             {status ? (
                 <div>
                     <p>Connected</p>

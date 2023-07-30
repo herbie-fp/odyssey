b7bc553be2ef7f745dcf88e1ac1fce5fb8039f8b
Edward Misback <elmisback@gmail.com>
2023-07-29 17:17:55 -0700

    Add Point files and show selected point on hover.

---
+++ src/Point.tsx
@@ -0,0 +1,26 @@
+import './Point.css'
+// React component
+// encapsulates: 
+// varnames.map((varname, i) => (
+//   <div className="selected-point-variable" key={i}>
+//   <div className="selected-point-variable-name">{varname}:</div>
+//   <div className="selected-point-variable-value">{selectedPoint?.[i]}</div>
+// </div>
+// ))
+
+// takes a dictionary of variable names to values
+// and renders them as x: 1, y: 2, z: 3 etc.
+export function Point({values}: { [varname: string]: number }) {
+  return (
+    <div className="point">
+      {
+        Object.entries(values).map(([varname, value]) => (
+          <div className="point-variable" key={varname}>
+            <div className="point-variable-name">{varname}:</div>
+            <div className="point-variable-value">{value}</div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}

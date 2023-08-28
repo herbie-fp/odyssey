fea8e9be959372c8a35dff665afe2787bfcb7729
Rich Chen <richchen2002@gmail.com>
2023-08-27 18:42:36 -0700

    Internally update the tree structure for derivation history

--- src/DerivationComponent.tsx
+++ src/DerivationComponent.tsx
@@ -3,14 +3,35 @@ import 'katex/dist/katex.min.css';
 import Latex from 'react-latex-next';
 
 import './DerivationComponent.css';
+import { Derivation } from './HerbieTypes';
 
-const DerivationComponent = () => { 
+const DerivationComponent = () => {
   const [derivations, setDerivations] = contexts.useGlobal(contexts.DerivationsContext)
   const [selectedExprId, setSelectedExprId] = contexts.useGlobal(contexts.SelectedExprIdContext)
-  
+
   const selectedDerivation = derivations.find(d => d.id === selectedExprId)
   if (!selectedDerivation) {
     return <div>Could not find expression with id {selectedExprId}</div>
+  }
+
+  let currentExpressionId: number | undefined = selectedDerivation.id
+  let expressionAncestry = []
+
+  // While the current expression is a root expression
+  while (currentExpressionId !== undefined) {
+    // Find its parent
+    const nextDerivation = derivations.find(d => d.id === currentExpressionId)
+
+    // If parent is not found, break
+    if (nextDerivation === undefined) {
+      break
+    }
+
+    // Otherwise, add it to the ancestry
+    expressionAncestry.push(nextDerivation.id)
+
+    // Examine the parent as the next expression
+    currentExpressionId = nextDerivation.parentId
   }
 
   return (

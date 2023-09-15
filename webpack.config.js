f8e2eb8b986306e5fab878cf3a3d4f50a15c5540
Edward Misback <misback@cs.washington.edu>
2023-09-15 10:13:21 -0700

    Add knitting stuff

--- webpack.config.js
+++ webpack.config.js
@@ -91,4 +91,49 @@ const extensionConfig = {
     level: "log", // enables logging required for problem matchers
   },
 };
-module.exports = [ extensionConfig, webviewConfig ];
+
+/** @type WebpackConfig */
+const knittingConfig = {  
+  mode: 'development', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  target: 'web', // vscode extensions run in a webworker context 📖 -> https://webpack.js.org/configuration/target/
+  entry: {
+    webview: ['./src/knitting/webview2.tsx']
+  }, // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist', 'knitting'),
+    filename: '[name].bundle.js',
+    //publicPath: 'http://localhost:3000/'
+  },
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js', '.tsx', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
+};
+
+module.exports = [ extensionConfig, webviewConfig, knittingConfig ];

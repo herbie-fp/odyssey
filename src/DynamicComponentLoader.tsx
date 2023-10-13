7216e87729308c6229f4cc48469d385c09e97055
Rich Chen <richchen2002@gmail.com>
2023-10-13 09:14:50 -0700

    Update: attempted fix for Babel

--- src/DynamicComponentLoader.tsx
+++ src/DynamicComponentLoader.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-// @ts-ignore
-import Babel from '@babel/standalone';
+const babel = require('@babel/core');
 
 interface DynamicComponentLoaderProps {
     componentString: string;
@@ -16,22 +15,19 @@ function DynamicComponentLoader(props: DynamicComponentLoaderProps) {
         transpileAndLoad(componentString);
     }, [componentString]);
 
-    const transpileAndLoad = (code) => {
+    const transpileAndLoad = (code: string) => {
         try {
-            const transpiledCode = Babel.transform(code, {
+            const transpiledCode = babel.transform(code, {
                 presets: ['react'],
             }).code;
-    
-            console.log('Transpiled code:', transpiledCode);
-    
-            const dynamicModule = {};
+
+            const dynamicModule: any = {};
             eval(transpiledCode + '\nmodule.exports = Component;');
             setComponent(() => dynamicModule.exports);
         } catch (error) {
             console.error("Failed to transpile and load the component:", error);
         }
     };
-    
 
     if (!Component) return null;
 

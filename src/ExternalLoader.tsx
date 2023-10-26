80edaa10c052c1b5dd2c5fe34ea37fd121f23a4c
Rich Chen <richchen2002@gmail.com>
2023-10-25 20:57:31 -0700

    Some external component work

--- src/ExternalLoader.tsx
+++ src/ExternalLoader.tsx
@@ -1,36 +1,36 @@
-// import React, { useEffect, useRef } from 'react';
-// import ReactDOM from 'react-dom';
+import React, { useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 
-// const ExternalLoader: React.FC<{ componentString: string }> = ({ componentString }) => {
-//   return new Function('React', 'return ' + componentString)(React)()
-// };
-
-// export default ExternalLoader;
-
-import React, { useEffect, useState } from 'react';
-
-const ExternalLoader = () => {
-  const [externalComponent, setExternalComponent] = useState('<div>Loading...</div>');
-
-  useEffect(() => {
-    //@ts-ignore
-    window.vscode.postMessage(JSON.stringify({
-      command: 'loadExternal',
-      file: 'external/ExternalComponent.tsx'
-    }));
-  })
-
-  window.addEventListener('message', event => {
-    //@ts-ignore
-    setExternalComponent(event.data.fileContents);
-    console.log(event.data.fileContents)
-  });
-
-  return (
-    <div dangerouslySetInnerHTML={{ __html: externalComponent }}>
-
-    </div>
-  );
+const ExternalLoader: React.FC<{ componentString: string }> = ({ componentString }) => {
+  return new Function('React', 'return ' + componentString)(React)()
 };
 
-export { ExternalLoader };
+export default ExternalLoader;
+
+// import React, { useEffect, useState } from 'react';
+
+// const ExternalLoader = () => {
+//   const [externalComponent, setExternalComponent] = useState('<div>Loading...</div>');
+
+//   useEffect(() => {
+//     //@ts-ignore
+//     window.vscode.postMessage(JSON.stringify({
+//       command: 'loadExternal',
+//       file: 'external/ExternalComponent.tsx'
+//     }));
+//   })
+
+//   window.addEventListener('message', event => {
+//     //@ts-ignore
+//     setExternalComponent(event.data.fileContents);
+//     console.log(event.data.fileContents)
+//   });
+
+//   return (
+//     <div dangerouslySetInnerHTML={{ __html: externalComponent }}>
+
+//     </div>
+//   );
+// };
+
+// export { ExternalLoader };

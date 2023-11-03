f6daf39fdeb0f95fd5788a3d913cfa6a9c4293f5
Rich Chen <richchen2002@gmail.com>
2023-11-03 14:55:35 -0700

    Remove webpack test stuff (moved to separate repo)

--- src/ExternalLoader.tsx
+++ src/ExternalLoader.tsx
@@ -2,7 +2,22 @@ import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const ExternalLoader: React.FC<{ componentString: string }> = ({ componentString }) => {
-  return new Function('React', 'return ' + componentString)(React)()
+  const renderCode = () => {
+    // Create a script element to execute the code
+    const scriptElement = document.createElement('script');
+    scriptElement.type = 'text/javascript';
+    scriptElement.text = componentString;
+    document.head.appendChild(scriptElement);
+  };
+
+  return (
+    <div>
+      <button onClick={renderCode}>Render Code</button>
+      <div id="root"></div>
+    </div>
+  );
+
+  // return new Function('React', 'return ' + componentString)(React)()
 };
 
 export default ExternalLoader;
--- webpack-test/webpack-demo/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return <div>Hello, React and TypeScript!</div>;
-};
-
-export default App;

cd48ad2d149c06d28002c757c57ec961abf15951
Ben Wang <benwang3@cs.washington.edu>
2024-07-01 18:51:25 -0700

    cost column and pareto

---
+++ src/herbie/SpeedVersusAccuracyPareto.tsx
@@ -0,0 +1,139 @@
+import React from 'react';
+const Plot = require('@observablehq/plot')  // have to do this for ES modules for now
+
+
+interface SpeedVersusAccuracyParetoProps {
+    // Define your component props here
+}
+type Point = [number, number]
+
+async function plotParetoPoints (bits: number, initial_pt: Point, rest_pts: Point[]) {
+    // const bits = benchmark["bits"];
+
+    // The line differs from rest_pts in two ways:
+    // - We filter to the actual pareto frontier, in case points moved
+    // - We make a broken line to show the real Pareto frontier
+    let line: Point[] = [];
+    let last = null;
+
+    // create a new array with rest_pts sorted by cost
+    const sorted_pts = rest_pts.slice().sort((a, b) => b[0] - a[0]);
+    
+    for (let pt of sorted_pts) {
+        if (!last || pt[1] > last[1]) {
+            if (last) line.push([pt[0], last[1]]);
+            line.push([pt[0], pt[1]]);
+            last = pt;
+        }
+    }
+    console.debug('plotPareto', sorted_pts, line)
+
+    const out = Plot.plot({
+        marks: [
+            Plot.line(line, {
+                x: (d: Point) => initial_pt[0]/d[0],
+                y: (d: Point) => 1 - d[1]/bits,
+                stroke: "#00a", strokeWidth: 3, strokeOpacity: .6,
+            }),
+            Plot.dot(rest_pts, {
+                x: (d: Point) => initial_pt[0]/d[0],
+                y: (d: Point) => 1 - d[1]/bits,
+                fill: "#00a", r: 9,
+            }),
+            // Plot.dot([initial_pt], {
+            //     x: d => initial_pt[0]/d[0],
+            //     y: d => 1 - d[1]/bits,
+            //     stroke: "#d00", symbol: "square", strokeWidth: 2
+            // }),
+            // target_pts && Plot.dot(target_pts, {
+            //     x: d => initial_pt[0]/d[0],
+            //     y: d => 1 - d[1]/bits,
+            //     stroke: "#080", symbol: "circle", strokeWidth: 2
+            // }),
+        ].filter(x=>x),
+        marginBottom: 0,
+        marginRight: 0,
+        width: '800',
+        height: '300',
+        x: { line: true, nice: true, tickFormat: (c : string) => c + "×" },
+        y: { nice: true, line: true, domain: [0, 1], tickFormat: "%" },
+    })
+    return out
+}
+
+async function makeExampleSVG(color: string) {
+    // make a simple svg
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', '100');
+    svg.setAttribute('height', '100');
+    const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    rect.setAttribute('x', '10');
+    rect.setAttribute('y', '10');
+    rect.setAttribute('width', '80');
+    rect.setAttribute('height', '80');
+    rect.setAttribute('fill', color);
+    svg.appendChild(rect);
+    return svg;
+}
+
+import * as Contexts from './HerbieContext';
+
+const SpeedVersusAccuracyPareto: React.FC<SpeedVersusAccuracyParetoProps> = (props) => {
+    const [color, setColor] = React.useState('red');
+    // Access the global expressions field
+    const [expressions, setExpressions] = Contexts.useGlobal(Contexts.ExpressionsContext);
+    // get the spec
+    const [spec, setSpec] = Contexts.useGlobal(Contexts.SpecContext);
+    // get the costs and errors for each expression
+    const [costs, setCosts] = Contexts.useGlobal(Contexts.CostContext);
+    const [analyses, setAnalyses] = Contexts.useGlobal(Contexts.AnalysesContext);
+    // convert error to percent accuracy
+    const errorToAccuracy = (error: number) => error// 1 - error / 64;
+    // get the spec's expression
+    const naiveExpression = expressions.find(e => e.text === spec.expression);
+    if (naiveExpression === undefined) {
+        return <div>Naive expression not found</div>
+    }
+    // get the cost and accuracy for the naiveExpression
+    const naiveCost = costs.find(c => c.expressionId === naiveExpression.id)?.cost;
+    const naiveError = analyses.find(a => a.expressionId === naiveExpression.id)?.data.meanBitsError;
+    if (naiveCost === undefined || naiveError === undefined) {
+        return <div>Naive cost or error not found</div>
+    }
+    const naiveAccuracy = errorToAccuracy(naiveError);
+    // get the ids of the selected expressions
+    const [selectedExprIds, setSelectedExprIds] = Contexts.useGlobal(Contexts.CompareExprIdsContext);
+
+    // iterate through each expression to find its cost and accuracy and store them in an array as as [cost, accuracy] tuple
+    const points = selectedExprIds.map(id => {
+        const expression = expressions.find(e => e.id === id);
+        if (expression === undefined) {
+            return [0, 0] as Point;
+        }
+        const cost = costs.find(c => c.expressionId === expression.id)?.cost;
+        const error = analyses.find(a => a.expressionId === expression.id)?.data.meanBitsError;
+        if (cost === undefined || error === undefined) {
+            return [0, 0] as Point;
+        }
+        const accuracy = errorToAccuracy(error);
+        return [cost, accuracy] as Point;
+    });
+
+    return (
+        <div>
+            {/* Use a ref to update the svg*/}
+            <svg viewBox="0 -25 840 360" ref={async (svg) => {
+                if (!svg) {
+                    return
+                  }
+                  svg.innerHTML = ''
+                  const plot = await plotParetoPoints(64, [naiveCost, naiveAccuracy], points);
+                  ([...plot.children]).map(c => svg.appendChild(c))
+            }
+        }></svg>
+        </div>
+    );
+};
+
+export default SpeedVersusAccuracyPareto;
+

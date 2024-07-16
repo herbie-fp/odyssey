36cebb131e3a894243b783b6058d00c5a7872a02
Edward Misback <misback@cs.washington.edu>
2024-07-16 13:01:03 -0700

    Add npm install

--- server/install.sh
+++ server/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+npm install
 node download-and-unzip.js https://github.com/herbie-fp/odyssey/releases/download/fptaylor-component/fptaylor-dist.zip fptaylor
 chmod +x fptaylor/linux/fptaylor-compiled/fptaylor
 node download-and-unzip.js https://github.com/herbie-fp/odyssey/releases/download/fptaylor-component/fpbench-dist.zip fpbench

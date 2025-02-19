75119a3076836b7257b82d14b261630bc04e7ce9
Rich Chen <richchen2002@gmail.com>
2025-02-19 12:50:28 -0800

    Alternatives curl test script

---
+++ src/test/curl-alternatives-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+URL="https://herbie.uwplse.org/demo/api/alternatives"
+BODY='{ "formula": "(FPCore (x) :spec (sqrt (* x x)) (sqrt (* x x)))", "sample": [ [ [-3.433673523436811e+284], 3.433673523436811e+284 ], [ [-5.949208088016812e+185], 5.949208088016812e+185 ], [ [4.808590949423921e-41], 4.808590949423921e-41 ], [ [-1.9989002899750099e-50], 1.9989002899750099e-50 ] ] }'
+
+curl -X POST "$URL" \
+     -H "Content-Type: application/json" \
+     -d "$BODY"

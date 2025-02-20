49a49a8a8025186ab6ca8ed44a418d58b54c1d5c
Rich Chen <richchen2002@gmail.com>
2025-02-19 17:36:46 -0800

    Use localhost

--- src/test/curl-alternatives-test.sh
+++ src/test/curl-alternatives-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="https://herbie.uwplse.org/demo/api/alternatives"
+URL="http://localhost:8000/demo/api/alternatives"
 BODY='{ "formula": "(FPCore (x) :spec (sqrt (* x x)) (sqrt (* x x)))", "sample": [ [ [-3.433673523436811e+284], 3.433673523436811e+284 ], [ [-5.949208088016812e+185], 5.949208088016812e+185 ], [ [4.808590949423921e-41], 4.808590949423921e-41 ], [ [-1.9989002899750099e-50], 1.9989002899750099e-50 ] ] }'
 
 curl -X POST "$URL" \

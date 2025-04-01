27af5833f6ec305d992470fbd94f0fe89166d048
Rich Chen <richchen2002@gmail.com>
2025-03-31 21:35:52 -0700

    Convert to functional React

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,40 +1,18 @@
-import { Component, ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { showErrorToast } from "./ErrorToast";
 
-//TODO : Convert to modern React
+const ErrorBoundary = ({ children }: { children: ReactNode }) => {
+    useEffect(() => {
+        const handleError = (event: ErrorEvent) => {
+            console.log('caught error', event);
+            showErrorToast(event.message);
+        };
 
-class ErrorBoundary extends Component<{ children: ReactNode }, { hasError: boolean }> {
-    constructor(props: any) {
-        super(props);
-        this.state = { hasError: false };
-        console.log("adding event listener")
-        window.addEventListener("error", (event) => {
-            console.log('caught error', event)
-            showErrorToast(event.message)
-            //vscode.postMessage(JSON.stringify({ command: 'error', error: event.error?.toString ? event.error.toString() : (JSON.stringify(event.error) + '\\n' + 'Message:' + event.message) }))
-        })
-        // window.addEventListener("unhandledrejection", (event) => {
-        //     console.log('caught unhandledrejection', event)
-        //     showErrorToast(JSON.stringify(event))
-        //     //vscode.postMessage(JSON.stringify({ command: 'error', error: event.reason.stack }))
-        // })
-    }
+        window.addEventListener("error", handleError);
+        return () => window.removeEventListener("error", handleError);
+    }, []);
 
-    static getDerivedStateFromError() {
-        return { hasError: true };
-    }
-
-    componentDidCatch(error: Error) {
-        showErrorToast(error.message || 'An unexpected error occurred.');
-        console.error('Caught by ErrorBoundary:', error);
-    }
-
-    render() {
-        if (this.state.hasError) {
-            return null; 
-        }
-        return this.props.children;
-    }
-}
+    return <>{children}</>;
+};
 
 export default ErrorBoundary;
